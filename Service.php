0da38694469bc6fd1aeff6e0f5a177040f87c75c
farizluqman <fariz.fnb@gmail.com>
2017-10-07 08:07:43 +0800

    Release candidate 0.5 - first release

---
+++ Service.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * StupidlySimple Framework - A PHP Framework For Lazy Developers
+ *
+ * Copyright (c) 2017 Fariz Luqman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package     StupidlySimple
+ * @author      Fariz Luqman <fariz.fnb@gmail.com>
+ * @copyright   2017 Fariz Luqman
+ * @license     MIT
+ * @link        https://stupidlysimple.github.io/
+ */
+namespace Simplyfier;
+
+/**
+ * Class Service
+ * Service is where all the applications written by user resides, for both
+ * input and output processing
+ *
+ * @package Simplyfier
+ *
+ * @since 0.5.0
+ */
+class Service {
+    /**
+     * Contains the object of instantiation of the Service class
+     * @static
+     * @var object
+     *
+     * @since 0.5.0
+     */
+    private static $serviceObject;
+
+    /**
+     * @var array
+     */
+    private $config = '';
+
+    /**
+     * @static
+     * @return Service|object
+     *
+     * @since 0.5.0
+     */
+    public static function loadServices()
+    {
+        if(isset(self::$serviceObject) === false){
+            self::$serviceObject = new self;
+        }
+
+        self::$serviceObject->prepare();
+
+        return self::$serviceObject;
+    }
+
+    /**
+     * Object preparer
+     *
+     * @since 0.5.0
+     */
+    private function prepare(){
+        if(isset($config) === false){
+            $this->config = Config::get('services');
+        }
+
+        foreach($this->config as $className => $varName){
+            $this->$varName = new $className;
+        }
+    }
+}
